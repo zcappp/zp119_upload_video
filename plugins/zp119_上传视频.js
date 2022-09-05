10b5b9f4a185ad1eaa4fa3bdfdcecbc4b2666441
zc <zc-yun@outlook.com>
2022-09-05 12:08:06 +0800

    1

--- plugins/zp119_上传视频.js
+++ plugins/zp119_上传视频.js
@@ -10,7 +10,9 @@ function render(ref) {
         {img || ref.progress ? <div>{ref.progress}</div> : <div>{camera}<label>{ref.props.label || "上传视频"}</label></div>}
         {ref.video ? <video src={ref.video}/> : (img ? <img src={img}/> : "")}
         {!!img && <i className="zplaybtn"/>}
-        {!!img && <i className="zp119rm" onClick={e => {e.stopPropagation(); ref.setForm(ref.props.dbf, "")}}>{remove}</i>}
+        {!!img && <svg onClick={e => {e.stopPropagation(); ref.setForm(ref.props.dbf, ""); ref.exc('render()')}} className="zp119rm zsvg" viewBox="64 64 896 896"><path d={remove}/></svg>}
+        {!!ref.props.url && <span onClick={() => url(ref)}>URL</span>}
+        {ref.modal}
     </React.Fragment>
 }
 
@@ -51,6 +53,44 @@ function clean(ref) {
     ref.container.classList.remove("uploading")
 }
 
+function url(ref) {
+    ref.modal = <div className="zmodals">
+        <div className="zmask" onClick={() => close(ref)}/>
+        <div className="zmodal">
+            <svg onClick={() => close(ref)} className="zsvg" viewBox="64 64 896 896"><path d={remove}/></svg>
+            <div className="zmodal-hd">通过URL上传</div>
+            <div className="zmodal-bd"><input placeholder="输入视频URL" className="zinput"/></div>
+            <div className="zmodal-ft">
+                <div className="zbtn" onClick={() => close(ref)}>取消</div>
+                <div className="zbtn zprimary" onClick={() => upload(ref)}>上传</div>
+            </div>
+        </div>
+    </div>
+    ref.render()
+    setTimeout(() => $(".zp119 .zmodal input").focus(), 9)
+}
+
+function close(ref) {
+    ref.modal = ""
+    ref.render()
+}
+
+function upload(ref) {
+    const { exc } = ref
+    let url = $(".zp119 .zmodal input").value
+    if (!url) return exc('alert("请输入视频URL")')
+    exc('info("正在上传，请稍候")')
+    close(ref)
+    exc('$resource.uploads(urls, "v")', { urls: [url] }, r => {
+        if (!r || r.ng.length) exc(`alert("上传出错了", reason)`, { reason: r ? r.ng[0].reason : "" })
+        if (r.arr.length) {
+            ref.setForm(ref.props.dbf, r.arr[0].url)
+            if (ref.props.onSuccess) exc(ref.props.onSuccess, { ...ref.ctx, $ext_ctx: ref.ctx, ...r.arr[0] }, () => exc("render()"))
+            exc('render()')
+        }
+    })
+}
+
 $plugin({
     id: "zp119",
     props: [{
@@ -70,10 +110,14 @@ $plugin({
         prop: "label",
         type: "text",
         label: "[上传视频]文本"
+    }, {
+        prop: "url",
+        type: "switch",
+        label: "允许通过URL上传"
     }],
     render,
     css
 })
 
-const camera = <svg className="zsvg zp119camera" viewBox="64 64 896 896"><path d="M912 302.3L784 376V224c0-35.3-28.7-64-64-64H128c-35.3 0-64 28.7-64 64v576c0 35.3 28.7 64 64 64h592c35.3 0 64-28.7 64-64V648l128 73.7c21.3 12.3 48-3.1 48-27.6V330c0-24.6-26.7-40-48-27.7zM712 792H136V232h576v560zm176-167l-104-59.8V458.9L888 399v226zM208 360h112c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H208c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8z"/></svg>
-const remove = <svg className="zsvg" viewBox="64 64 896 896"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"/></svg>
+const camera = <svg className="zsvg zp119camera" viewBox="0 0 1024 1024"><path d="M384 128l-78.933333 85.333333L170.666667 213.333333c-46.933333 0-85.333333 38.4-85.333333 85.333333l0 512c0 46.933333 38.4 85.333333 85.333333 85.333333l682.666667 0c46.933333 0 85.333333-38.4 85.333333-85.333333L938.666667 298.666667c0-46.933333-38.4-85.333333-85.333333-85.333333l-134.4 0L640 128 384 128zM512 768c-119.333333 0-213.333333-96-213.333333-213.333333s96-213.333333 213.333333-213.333333 213.333333 96 213.333333 213.333333S629.333333 768 512 768zM512 554.666667m-136.533333 0a6.4 6.4 0 1 0 273.066667 0 6.4 6.4 0 1 0-273.066667 0Z"/></svg>
+const remove = "M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"
