0505f0d57c90b75861a3d3827165a633017654dd
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-05-13 09:26:32 +0800

    1

--- src/zp119_上传视频.js
+++ src/zp119_上传视频.js
@@ -12,7 +12,7 @@ function render(ref) {
         {!!ref.progress && <div className="zp119progress">{ref.progress}</div>}
         {ref.video || (video && !video.endsWith("mp4")) ? <video src={ref.video}/> : (video ? <img onClick={() => preview(ref, video)} src={video + "?x-oss-process=video/snapshot,m_fast,t_5000,w_0,ar_auto"}/> : "")}
         {!!video && <i className="zplaybtn" onClick={() => preview(ref, video)}/>}
-        {!!video && <svg onClick={e => {e.stopPropagation(); ref.setForm(props.dbf, ""); ref.exc('render()')}} className="zp119rm zsvg" viewBox="64 64 896 896"><path d={remove}/></svg>}
+        {!!video && <i onClick={e => {e.stopPropagation(); ref.setForm(props.dbf, ""); ref.exc('render()')}} className="zdel zp119rm"/>}
         {!!props.url && !ref.video && <span onClick={() => popUrl(ref)}>URL</span>}
         {ref.modal}
     </React.Fragment>
@@ -59,7 +59,7 @@ function popUrl(ref) {
     ref.modal = <div className="zmodals">
         <div className="zmask" onClick={() => close(ref)}/>
         <div className="zmodal">
-            <svg onClick={() => close(ref)} className="zsvg x" viewBox="64 64 896 896"><path d={remove}/></svg>
+            <i onClick={() => close(ref)} className="zdel"/>
             <h3 className="hd">通过URL上传</h3>
             <div className="bd"><input placeholder="输入视频URL" className="zinput"/></div>
             <div className="ft">
@@ -80,7 +80,7 @@ function preview(ref, video) {
     ref.modal = <div className="zmodals">
         <div className="zmask" onClick={() => close(ref)}/>
         <div className="zmodal">
-            <svg onClick={() => close(ref)} className="zsvg x" viewBox="64 64 896 896"><path d={remove}/></svg>
+            <i onClick={() => close(ref)} className="zdel"/>
             <h3 className="hd">{ref.props.dbf}</h3>
             <div className="zcenter"><video src={video} preload="metadata" controls/></div>
         </div>
@@ -140,4 +140,3 @@ $plugin({
 })
 
 const camera = <svg className="zsvg zp119camera" viewBox="64 64 896 896"><path d="M912 302.3L784 376V224c0-35.3-28.7-64-64-64H128c-35.3 0-64 28.7-64 64v576c0 35.3 28.7 64 64 64h592c35.3 0 64-28.7 64-64V648l128 73.7c21.3 12.3 48-3.1 48-27.6V330c0-24.6-26.7-40-48-27.7zM712 792H136V232h576v560zm176-167l-104-59.8V458.9L888 399v226zM208 360h112c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H208c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8z"/></svg>
-const remove = "M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"
